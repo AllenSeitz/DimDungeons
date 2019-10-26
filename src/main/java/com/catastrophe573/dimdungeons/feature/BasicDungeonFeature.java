f7a6cdbf69b37b78cb915bc74fdc9fa353490a7e
Allen <allenseitz@gmail.com>
2019-10-26 03:21:59 -0400

    Removed unused imports

--- src/main/java/com/catastrophe573/dimdungeons/feature/BasicDungeonFeature.java
+++ src/main/java/com/catastrophe573/dimdungeons/feature/BasicDungeonFeature.java
@@ -13,7 +13,6 @@ import com.mojang.datafixers.Dynamic;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ILivingEntityData;
 import net.minecraft.entity.MobEntity;
@@ -36,9 +35,6 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.MutableBoundingBox;
 import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.util.text.TextComponent;
-import net.minecraft.util.text.TextComponentUtils;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.gen.ChunkGenerator;
@@ -48,7 +44,6 @@ import net.minecraft.world.gen.feature.NoFeatureConfig;
 import net.minecraft.world.gen.feature.template.PlacementSettings;
 import net.minecraft.world.gen.feature.template.Template;
 import net.minecraft.world.gen.feature.template.TemplateManager;
-import net.minecraftforge.server.command.TextComponentHelper;
 
 public class BasicDungeonFeature extends Feature<NoFeatureConfig>
 {
