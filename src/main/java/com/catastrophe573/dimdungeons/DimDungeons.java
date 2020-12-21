d00e30d89aaf3af05b764ff448d14ffb8ebb953c
Allen <allenseitz@gmail.com>
2020-12-20 20:48:25 -0500

    Misc Cleanup

--- src/main/java/com/catastrophe573/dimdungeons/DimDungeons.java
+++ src/main/java/com/catastrophe573/dimdungeons/DimDungeons.java
@@ -9,8 +9,6 @@ import net.minecraft.tileentity.TileEntityType;
 import net.minecraft.util.RegistryKey;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.registry.Registry;
-import net.minecraft.world.Dimension;
-import net.minecraft.world.DimensionType;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.ChunkGenerator;
 import net.minecraftforge.common.MinecraftForge;
