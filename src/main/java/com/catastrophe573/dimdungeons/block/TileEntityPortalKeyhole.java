36cc1e611cd1edc5273eaf12cd8a19496257cfe1
Allen <allenseitz@gmail.com>
2022-05-01 17:42:14 -0400

    Removing an info() comment that I don't want in anyone's logs anymore.

--- src/main/java/com/catastrophe573/dimdungeons/block/TileEntityPortalKeyhole.java
+++ src/main/java/com/catastrophe573/dimdungeons/block/TileEntityPortalKeyhole.java
@@ -70,7 +70,7 @@ public class TileEntityPortalKeyhole extends BlockEntity
 	    int j = chunk % 8;
 	    ChunkPos cpos = new ChunkPos(((int) buildX / 16) + i + 4, ((int) buildZ / 16) + j + 4); // the arbitrary +4 is to make the dungeons line up with vanilla maps
 
-	    DimDungeons.logMessageInfo("Ticking BUILD_STEP: " + buildStep + ", building chunk " + chunk);
+	    //DimDungeons.logMessageInfo("Ticking BUILD_STEP: " + buildStep + ", building chunk " + chunk);
 	    placedRoom = DungeonPlacement.buildRoomAboveSign(dungeonWorld, cpos, genData);
 	}
 
