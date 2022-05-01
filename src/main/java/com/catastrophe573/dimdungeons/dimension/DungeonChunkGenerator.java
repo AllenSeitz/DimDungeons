71c7ae393b3568b7b39f3476a11f718d6d271089
Allen <allenseitz@gmail.com>
2022-05-01 19:41:41 -0400

    Technical changes to the chunk generator.
    Technically, these changes do nothing. But they might block certain structures from generating. (The base still does not generate and I do not know why.)

--- src/main/java/com/catastrophe573/dimdungeons/dimension/DungeonChunkGenerator.java
+++ src/main/java/com/catastrophe573/dimdungeons/dimension/DungeonChunkGenerator.java
@@ -1,7 +1,6 @@
 package com.catastrophe573.dimdungeons.dimension;
 
 import java.util.List;
-import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 
@@ -13,23 +12,23 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Registry;
+import net.minecraft.core.RegistryAccess;
 import net.minecraft.world.level.NoiseColumn;
-import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LevelHeightAccessor;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraft.world.level.chunk.ChunkAccess;
 import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.minecraft.world.level.levelgen.FlatLevelSource;
+import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraft.world.level.levelgen.flat.FlatLevelGeneratorSettings;
 import net.minecraft.world.level.levelgen.structure.StructureSet;
-import net.minecraft.world.level.levelgen.GenerationStep.Carving;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureManager;
 import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.level.levelgen.Heightmap.Types;
 import net.minecraft.world.level.levelgen.blending.Blender;
 import net.minecraft.server.level.WorldGenRegion;
 import net.minecraft.world.level.StructureFeatureManager;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.biome.BiomeManager;
 import net.minecraft.world.level.biome.Climate.Sampler;
 import net.minecraft.world.level.biome.FixedBiomeSource;
@@ -43,7 +42,6 @@ public final class DungeonChunkGenerator extends ChunkGenerator
     });
 
     private final FlatLevelGeneratorSettings settings;
-    private long worldSeed = 0;
 
     public DungeonChunkGenerator(Registry<StructureSet> p_209099_, FlatLevelGeneratorSettings p_209100_)
     {
@@ -59,7 +57,7 @@ public final class DungeonChunkGenerator extends ChunkGenerator
     @OnlyIn(Dist.CLIENT)
     public ChunkGenerator withSeed(long p_230349_1_)
     {
-	worldSeed = p_230349_1_;
+	//worldSeed = p_230349_1_;
 	return this;
     }
 
@@ -68,33 +66,12 @@ public final class DungeonChunkGenerator extends ChunkGenerator
 	return this.settings;
     }
 
-    // I don't know what this does. I copied it from the vanilla code.
+    // in vanilla this would build the flat worldgen layers, I think
     @Override
     public CompletableFuture<ChunkAccess> fillFromNoise(Executor p_187748_, Blender p_187749_, StructureFeatureManager p_187750_, ChunkAccess p_187751_)
     {
-	List<BlockState> list = this.settings.getLayers();
-	BlockPos.MutableBlockPos blockpos$mutableblockpos = new BlockPos.MutableBlockPos();
-	Heightmap heightmap = p_187751_.getOrCreateHeightmapUnprimed(Heightmap.Types.OCEAN_FLOOR_WG);
-	Heightmap heightmap1 = p_187751_.getOrCreateHeightmapUnprimed(Heightmap.Types.WORLD_SURFACE_WG);
-
-	for (int i = 0; i < Math.min(p_187751_.getHeight(), list.size()); ++i)
-	{
-	    BlockState blockstate = list.get(i);
-	    if (blockstate != null)
-	    {
-		int j = p_187751_.getMinBuildHeight() + i;
-
-		for (int k = 0; k < 16; ++k)
-		{
-		    for (int l = 0; l < 16; ++l)
-		    {
-			p_187751_.setBlockState(blockpos$mutableblockpos.set(k, j, l), blockstate, false);
-			heightmap.update(k, j, l, blockstate);
-			heightmap1.update(k, j, l, blockstate);
-		    }
-		}
-	    }
-	}
+	// I don't know why this function isn't being called, but it doesn't really matter
+	makeBase(p_187751_);
 
 	return CompletableFuture.completedFuture(p_187751_);
     }
@@ -135,15 +112,11 @@ public final class DungeonChunkGenerator extends ChunkGenerator
 	}));
     }
 
-    public void makeBase(LevelAccessor worldIn, ChunkAccess chunkIn)
+    //public void makeBase(LevelAccessor worldIn, ChunkAccess chunkIn)
+    public void makeBase(ChunkAccess chunkIn)
     {
-	// I still want a random seed, like the overworld, for use in structures
-	Random randomSeed = worldIn.getRandom();
-
-	// 1.14 seed changing logic
 	int x = chunkIn.getPos().x;
 	int z = chunkIn.getPos().z;
-	randomSeed.setSeed((worldSeed + (long) (x * x * 4987142) + (long) (x * 5947611) + (long) (z * z) * 4392871L + (long) (z * 389711) ^ worldSeed));
 
 	// first generate a superflat world - sandstone where dungeons can appear, and void otherwise
 	if (DungeonPlacement.isDungeonChunk(x, z))
@@ -201,19 +174,28 @@ public final class DungeonChunkGenerator extends ChunkGenerator
     }
 
     @Override
-    public void applyCarvers(WorldGenRegion p_187691_, long p_187692_, BiomeManager p_187693_, StructureFeatureManager p_187694_, ChunkAccess p_187695_, Carving p_187696_)
+    public void applyCarvers(WorldGenRegion p_187691_, long p_187692_, BiomeManager p_187693_, StructureFeatureManager p_187694_, ChunkAccess p_187695_, GenerationStep.Carving p_187696_)
     {
 	// intentionally do nothing!
     }
 
-    @SuppressWarnings("deprecation")
+    //@Override
+    //public void applyBiomeDecoration(WorldGenLevel p_187712_, ChunkAccess p_187713_, StructureFeatureManager p_187714_)
+    //{
+    // intentionally do nothing!
+    //}
+
     @Override
+    // I do not know why this isn't being called in 1.18 
     public void buildSurface(WorldGenRegion p_187697_, StructureFeatureManager p_187698_, ChunkAccess p_187699_)
     {
-	ServerLevel world = p_187697_.getLevel();
-	makeBase(world, p_187699_);
-
 	// no decoration! no structures!
+    }
+
+    @Override
+    public void createStructures(RegistryAccess p_62200_, StructureFeatureManager p_62201_, ChunkAccess p_62202_, StructureManager p_62203_, long p_62204_)
+    {
+	// intentionally do nothing!
     }
 
     @Override
