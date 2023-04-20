5dd5dddc7988e369d1fe71061778cc5927e9c511
Catastrophe573 <allenseitz@gmail.com>
2023-04-20 00:33:08 -0400

    Version up to 174

--- src/main/java/com/catastrophe573/dimdungeons/DimDungeons.java
+++ src/main/java/com/catastrophe573/dimdungeons/DimDungeons.java
@@ -76,7 +76,7 @@ public class DimDungeons
 
 		ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, DungeonConfig.SERVER_SPEC);
 		ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, DungeonConfig.CLIENT_SPEC);
-		ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, DungeonConfig.COMMON_SPEC, "dimdungeons-common-r152.toml");
+		ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, DungeonConfig.COMMON_SPEC, "dimdungeons-common-r174.toml");
 	}
 
 	private void doCommonStuff(final FMLCommonSetupEvent event)
