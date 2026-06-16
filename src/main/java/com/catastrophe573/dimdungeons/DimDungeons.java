197f860346bbc9ea49e9308457169f14e8143715
Catastrophe573 <allenseitz@gmail.com>
2026-06-15 23:58:40 -0400

    Config version up

--- src/main/java/com/catastrophe573/dimdungeons/DimDungeons.java
+++ src/main/java/com/catastrophe573/dimdungeons/DimDungeons.java
@@ -94,9 +94,9 @@ public class DimDungeons
 		NeoForge.EVENT_BUS.register(eventHandler);
 		NeoForge.EVENT_BUS.addListener(PlayerDungeonEvents::onWorldTick);
 
-		container.registerConfig(ModConfig.Type.SERVER, DungeonConfig.SERVER_SPEC, "dimdungeons-server-r206.toml");
+		container.registerConfig(ModConfig.Type.SERVER, DungeonConfig.SERVER_SPEC, "dimdungeons-server-r222.toml");
 		container.registerConfig(ModConfig.Type.CLIENT, DungeonConfig.CLIENT_SPEC);
-		container.registerConfig(ModConfig.Type.COMMON, DungeonConfig.COMMON_SPEC, "dimdungeons-common-r206.toml");
+		container.registerConfig(ModConfig.Type.COMMON, DungeonConfig.COMMON_SPEC, "dimdungeons-common-r222.toml");
 	}
 
 	@SubscribeEvent
